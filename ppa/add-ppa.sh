5919be2d49f80241d76a360bcacd7a75da680cd9
Abhishek Verma <abhishek_verma@hotmail.com>
2017-04-15 18:19:10 +0530

    Updating ppa files with new links

--- ppa/add-ppa.sh
+++ ppa/add-ppa.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-
-# Atom-hackable text editor
 sudo add-apt-repository ppa:webupd8team/atom -y
-
-# chromium-browser
+sudo add-apt-repository ppa:ubuntuhandbook1/audacity -y
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
+sudo add-apt-repository ppa:thomas-schiex/blender -y
+sudo add-apt-repository ppa:webupd8team/brackets -y
 sudo add-apt-repository ppa:saiarcot895/chromium-dev -y
-
-# geary-team
+sudo add-apt-repository ppa:mail6543210/codeblocks -y
+sudo add-apt-repository ppa:pmjdebruijn/darktable-1-6-testing -y
+sudo add-apt-repository ppa:julenlanda/evolution -y
 sudo add-apt-repository ppa:geary-team/releases -y
-
-# Minitube
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y
 sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-
-# Neofetch
 sudo add-apt-repository ppa:dawidd0811/neofetch -y
-
-# Skype
 sudo add-apt-repository ppa:andykimpe/skype -y
-
-# Tomhack
+sudo add-apt-repository ppa:ev1l0rd/steamcmddl -y
+sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
+sudo add-apt-repository ppa:atareao/telegram -y
 sudo add-apt-repository ppa:tomahawk/ppa -y
-
-# Unity-tweak-tool
+sudo add-apt-repository ppa:webupd8team/tor-browser -y
 sudo add-apt-repository ppa:tribaal/unity-tweak-tool -y
-
-# vlc
+sudo add-apt-repository ppa:plushuang-tw/uget-stable -y
 sudo add-apt-repository ppa:n-muench/vlc2 -y
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+wget -O - http://deb.opera.com/archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://deb.opera.com/opera-stable/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
--- ppa/install-ppa.sh
+++ ppa/install-ppa.sh
@@ -29,29 +29,27 @@ else
     echo -ne '\n'
 fi
 
-# Atom-hackable text editor
 sudo apt-get install atom -y
-
-# chromium-browser
+sudo apt-get install audacity -y
+sudo apt-get install qbittorrent -y
+sudo apt-get install blender -y
+sudo apt-get install brackets -y
+sudo apt-get install chrome
 sudo apt-get install chromium-browser -y
-
-# geary-team
+sudo apt-get install codeblocks -y
+sudo apt-get install darktable -y
+sudo apt-get install evolution -y
 sudo apt-get install geary -y
-
-# Minitube
+sudo apt-get install gimp -y
 sudo apt-get install minitube -y
-
-# Neofetch
 sudo apt-get install neofetch -y
-
-# Skype
+sudo apt-get install opera
 sudo apt-get install skype -y
-
-# Tomhack
+sudo apt-get install steamcmddl -y
+sudo apt-get install sublime-text -y
+sudo apt-get update telegram -y
 sudo apt-get install tomahawk -y
-
-# Unity-tweak-tool
+sudo apt-get install tor-browser  -y
 sudo apt-get install unity-tweak-tool -y
-
-# vlc
+sudo apt-get install uget -y
 sudo apt-get install vlc -y
